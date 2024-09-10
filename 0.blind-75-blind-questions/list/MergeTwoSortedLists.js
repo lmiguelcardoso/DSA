9c5acde4ca52977870e1f952991d5693a24c42eb
Miguel Cardoso <miguel.cardoso@avanade.com>
2024-09-09 21:51:03 -0300

    Merge Two Sorted Lists

---
+++ 0.blind-75-blind-questions/list/MergeTwoSortedLists.js
@@ -0,0 +1,18 @@
+var mergeTwoLists = function (list1, list2) {
+    let dummyNode = { val: -1, next: null };
+    let current = dummyNode;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+    current.next = list1 || list2;
+
+    return dummyNode.next;
+};
